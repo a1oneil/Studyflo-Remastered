f059f93f17be5ed24a15050bd128910f0b966a3e
Arianna O'Neil <oneilarianna@gmail.com>
2020-07-10 14:06:14 -0400

    uploading all of my current files
    this is the foundation of the new application

---
+++ Studyflo/PageCell.swift
@@ -0,0 +1,36 @@
+//
+//  PageCell.swift
+//  Studyflo
+//
+//  Created by Arianna O'Neil on 7/10/20.
+//  Copyright © 2020 Arianna O'Neil. All rights reserved.
+//
+
+import UIKit
+
+class PageCell: UICollectionViewCell {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        setupViews()
+    }
+    
+    let imageView: UIImageView = {
+        let iv = UIImageView()
+        iv.contentMode = .scaleAspectFill
+        iv.backgroundColor = .yellow
+        iv.image = UIImage(named: "studyflo_1")
+        iv.clipsToBounds = true
+        return iv
+    }()
+    
+    func setupViews(){
+        addSubview(imageView)
+        imageView.anchorToTop(top: topAnchor, left: leftAnchor, bottom: bottomAnchor, right: rightAnchor)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
